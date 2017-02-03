e566c4939c47da01d9f05907e720ca7111a81d25
Wenyi Chen <wenyichen@outlook.com>
2017-02-03 12:05:55 -0800

    fixed home js

--- FrontEnd/index.php
+++ FrontEnd/index.php
@@ -9,18 +9,29 @@
 	 	<meta name="keywords" content="HTML,CSS,JavaScript">
 		<meta name="author" content="Zifan Yang">
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
-
-	</head>
-	<body>
-		<div id="common">
-			<?php include 'common.html';?>
-		</div>
-	
-		<div class="container">
-		
-		</div>
-	</body>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script type="text/javascript">
+            $( document ).ready(function() {
+                $('#common').load('common.html');
+            });
+        </script>
+    </head>
+    <body>
+        <div id="common"></div>
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-4">
+                <div class="panel panel-custom">
+                    <div class="panel-heading"><h3>Welcome</h3></div>
+                    <div class="panel-body">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
 
