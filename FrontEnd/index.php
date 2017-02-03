99d6432118215d106cc8d4b2d07df9ab40c2cbe8
p6668 <randall764992309@hotmail.com>
2017-02-02 22:46:34 -0800

    alright

--- FrontEnd/index.php
+++ FrontEnd/index.php
@@ -11,19 +11,12 @@
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-		<!-- jQuery library -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
-		<!-- Latest compiled JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script type="text/javascript">
-			$( document ).ready(function() {
-				$('#common').load('common.html');
-			});	
-		</script>
 	</head>
 	<body>
-		<div id="common"></div>
+		<div id="common">
+			<?php include 'common.html';?>
+		</div>
 	
 		<div class="container">
 		
