4a73abca896bde039c72fd485d0f075f69ca08cd
JimmyTani <JimmyTani@users.noreply.github.com>
2017-02-25 13:59:51 -0800

    merge verify

---
+++ SquadUCSD-CalCulated-s-nuke-testing-site/pages/searchresult.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <link rel="stylesheet" type="text/css" href="../css/search_result_entry.css"/>
+    <link rel="stylesheet" type="text/css" href="../css/common.css"/>
+<head>
+    <title>SquadUCSD</title>
+    <meta charset="utf-8">
+    <meta name="description" content="UCSD study group searching site">
+    <meta name="keywords" content="HTML,CSS,JavaScript">
+    <meta name="author" content="Zifan Yang">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+</head>
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-4">
+                <div class="panel panel-custom">
+                    <div class="panel-heading"><h4>FirstName LastName</h4></div>
+                    <div class="panel-body">
+                        <div class="basicinfo">
+                
+                      
+                       <label class="col-sm-4">Matched Classes: </label>
+                       </div>
+                        <br><br>
+                        <div class="buttons">
+                            <a href="#" class="btn btn-primary" role="button">&#9993 Message</a>
+                            <a href="./viewprofile.html" target="_blank" class="btn btn-info" role="button">View Profile</a>
+                            <a href="#" class="btn btn-success" role="button">Invite to Existing Group</a>
+                             <a href="#" class="btn btn-success" role="button">Invite to Form New Group</a>
+                             
+                                    <br>
+                        </div>
+                               
+                           
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
