cc499f178c172319474a2c23526dec47c2d4367b
yil667 <yil667@ucsd.edu>
2017-02-27 20:14:17 -0800

    relax

--- controller/viewProfileAction.php
+++ controller/viewProfileAction.php
@@ -1,13 +1,11 @@
 <?php
+header("Location: ../pages/index.php");
 
 include_once "loginController.php";
 include_once "viewProfileController.php";
 
-echo '<pre>';
-print_r($_POST); // for viewing it as an array
-var_dump($_POST); // for viewing all info of the array
-echo '</pre>';
-die();
+
+
 
 $id = $_GET['userid']; // fetches the user's id
 
