b32f2ad5adb115c89b239b794d3d86dd1fa9af10
Wenyi Chen <wenyichen@outlook.com>
2017-02-27 16:36:50 -0800

    fixed jumbotron

--- pages/index.php
+++ pages/index.php
@@ -41,7 +41,7 @@ include_once '../controller/startUserSession.php';
             <div class="jumbotron">
                 <h1 id="welcome">Welcome</h1>
 
-                <p class="col-sm-12">
+                <p>
                     This web app allows students to form study groups for the classes they are enrolled in. Groups are formed based on schedule compatibility and individual personalities and preferences. Groups can set a maximum size to allow however many people in. This can be changed any time.
                 </p>
             </div>
