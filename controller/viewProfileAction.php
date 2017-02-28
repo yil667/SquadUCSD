4a2a69f418a7c5fcc349f2f3489b44336d8c64af
yil667 <yil667@ucsd.edu>
2017-02-27 20:08:52 -0800

    relax

--- controller/viewProfileAction.php
+++ controller/viewProfileAction.php
@@ -3,6 +3,9 @@
 include_once "loginController.php";
 include_once "viewProfileController.php";
 
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
 $id = $_GET['userid']; // fetches the user's id
 
 $user = getUserObject($id);
