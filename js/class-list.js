159030ce57d00935e2505a3110eb38840070eaa2
yil667 <yil667@ucsd.edu>
2017-02-28 01:45:00 -0800

    password change added

--- js/class-list.js
+++ js/class-list.js
@@ -5518,24 +5518,23 @@ $(function() {
 "WCWP 500",
   ];
 
+function results(request,response){
+    var term = $.ui.autocomplete.escapeRegex(request.term)
+        , startsWithMatcher = new RegExp("^" + term, "i")
+        , startsWith = $.grep(listOfClasses, function(value) {
+        return startsWithMatcher.test(value.label || value.value || value);
+    })
+        , containsMatcher = new RegExp(term, "i")
+        , contains = $.grep(listOfClasses, function (value) {
+        return $.inArray(value, startsWith) < 0 &&
+            containsMatcher.test(value.label || value.value || value);
+    });
 
+    response(startsWith.concat(contains).slice(0,10));
+}
 
 $( "#class2" ).autocomplete({
-    source: function(request, response) {
-        var term = $.ui.autocomplete.escapeRegex(request.term)
-            , startsWithMatcher = new RegExp("^" + term, "i")
-            , startsWith = $.grep(listOfClasses, function(value) {
-            return startsWithMatcher.test(value.label || value.value || value);
-        })
-            , containsMatcher = new RegExp(term, "i")
-            , contains = $.grep(listOfClasses, function (value) {
-            return $.inArray(value, startsWith) < 0 &&
-                containsMatcher.test(value.label || value.value || value);
-        });
-
-        response(startsWith.concat(contains).slice(0,10));
-
-    }
+    source: results(request,response);
 });
 
 
