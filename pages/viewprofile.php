b238142864354dff0748df5bfb584cb78c39ccb4
yil667 <yil667@ucsd.edu>
2017-02-27 18:28:04 -0800

    group profile

--- pages/viewprofile.php
+++ pages/viewprofile.php
@@ -172,7 +172,7 @@ include_once '../controller/viewProfileAction.php';
             </div>
             <div class="modal-body">
 
-                <form>
+                <form action="../controller/registerFormAction.php" role="form" method="POST" id="messageform">
                     <div class="form-group">
                         <label name="message" id="message" for="groupselect">Select Group</label>
                         <select id="groupselect" name="groupselect">
@@ -183,11 +183,11 @@ include_once '../controller/viewProfileAction.php';
                         <label name="message" id="message" for="messageboxinvite">Message</label>
                         <textarea class="form-control" name="messageboxinvite" id="messageboxinvite" rows="3"></textarea>
                     </div>
+                    <div class="modal-footer">
+                        <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+                        <button class="btn btn-primary">Send Invite</button>
+                    </div>
                 </form>
-            </div>
-            <div class="modal-footer">
-                <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
-                <button class="btn btn-primary">Send Invite</button>
             </div>
         </div>
     </div>
