35087342890f7707c14e0b29e54daadb9a494380
yil667 <yil667@ucsd.edu>
2017-02-28 16:17:55 -0800

    password change added

--- js/profile-validation.js
+++ js/profile-validation.js
@@ -15,22 +15,23 @@ $().ready(function () {
                 maxlength: 200
             },
             class1: {
-                maxlength: 10
+                maxlength: 40,
+                notEqualToGroup: {'.form-control-class'}
             },
             class2: {
-                maxlength: 10
+                maxlength: 40
             },
             class3: {
-                maxlength: 10
+                maxlength: 40
             },
             class4: {
-                maxlength: 10
+                maxlength: 40
             },
             class5: {
-                maxlength: 10
+                maxlength: 40
             },
             class6: {
-                maxlength: 10
+                maxlength: 40
             }
         },
         // Specify validation error messages
@@ -43,22 +44,22 @@ $().ready(function () {
                 maxlength: "Your about should be less than 200 characters"
             },
             class1: {
-                maxlength: "Your class should be less than 10 characters"
+                maxlength: "Your class should be less than 40 characters"
             },
             class2: {
-                maxlength: "Your class should be less than 10 characters"
+                maxlength: "Your class should be less than 40 characters"
             },
             class3: {
-                maxlength: "Your class should be less than 10 characters"
+                maxlength: "Your class should be less than 40 characters"
             },
             class4: {
-                maxlength: "Your class should be less than 10 characters"
+                maxlength: "Your class should be less than 40 characters"
             },
             class5: {
-                maxlength: "Your class should be less than 10 characters"
+                maxlength: "Your class should be less than 40 characters"
             },
             class6: {
-                maxlength: "Your class should be less than 10 characters"
+                maxlength: "Your class should be less than 40 characters"
             }
         },
         // Make sure the form is submitted to the destination defined
@@ -66,5 +67,23 @@ $().ready(function () {
         submitHandler: function (form) {
             form.submit();
         }
+    });
+    $.validator.addMethod("notEqualToGroup", function (value, element, options) {
+        // get all the elements passed here with the same class
+        var elems = $(element).parents('form').find(options[0]);
+        // the value of the current element
+        var valueToCompare = value;
+        // count
+        var matchesFound = 0;
+        // loop each element and compare its value with the current value
+        // and increase the count every time we find one
+        $.each(elems, function () {
+            thisVal = $(this).val();
+            if (thisVal == valueToCompare) {
+                matchesFound++;
+            }
+        });
+        // count should be either 0 or 1 max
+        return this.optional(element) || matchesFound <= 1;
     });
 });
