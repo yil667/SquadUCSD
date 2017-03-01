e2556ed2344c9887ac0323f5f9d1ee3f25a5e6a9
yil667 <yil667@ucsd.edu>
2017-03-01 00:38:11 -0800

    fixed the www issue feels good man

--- controller/startUserSession.php
+++ controller/startUserSession.php
@@ -6,6 +6,7 @@ session_start();
 $isLoggedIn = isLoggedIn() ? 1 : 0;
 $firstName = $isLoggedIn ? getFirstName() : "";
 
+// forcefully redirects the website to contain "www" so the session variable doesn't behave unexpectedly
 if($_SERVER['HTTP_HOST'] != "localhost" && strpos($_SERVER['HTTP_HOST'], "www.") === false)
 {
     $actual_link = "http://www.$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
