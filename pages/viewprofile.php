a99bf4f642bf9d7a44264f76bb1d255708841cb8
p6668 <randall764992309@hotmail.com>
2017-03-01 17:17:34 -0800

    button spacing

--- pages/viewprofile.php
+++ pages/viewprofile.php
@@ -85,12 +85,9 @@ else {
             $('#email').html(email);
 
             var loggedInContent =
-                "<button type='button' class='btn btn-primary' role='button' data-toggle='modal' data-target='#messageModal'>Message" +
-                "</button>" +
-                "<button type='button' class='btn btn-success' role='button' data-toggle='modal' data-target='#inviteModal'>Invite to Existing Group" +
-                "</button>" +
-                "<button type='button' class='btn btn-success' role='button' data-toggle='modal' data-target='#formModal'>Invite to Form New Group" +
-                "</button>";
+                "<button type='button' class='btn btn-primary' role='button' data-toggle='modal' data-target='#messageModal'>Message</button>" +
+                "<button type='button' class='btn btn-success' role='button' data-toggle='modal' data-target='#inviteModal'>Invite to Existing Group</button>" +
+                "<button type='button' class='btn btn-success' role='button' data-toggle='modal' data-target='#formModal'>Invite to Form New Group</button>";
             var defaultContent = "";
             var selfProfile = "<button type='button' onclick=" + "location.href=window.location.href.replace('view','edit')" + " class='btn btn-primary'>Edit Profile</button>";
 
@@ -142,9 +139,10 @@ else {
                                 <p class="form-control-static" name="about" id="about"></p>
                             </div>
                         </div>
+                        <div class="form-group">
+                            <div class="text-center" id="buttons">
 
-                        <div class="button" id="buttons" name="buttons">
-                            <!-- contents here is displayed conditionally -->
+                            </div>
                         </div>
                     </form>
                 </div>
