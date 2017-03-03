ffc3db70dda760fb4a53047eba9ac00d6109dd0a
yil667 <yil667@ucsd.edu>
2017-03-03 14:13:25 -0800

    gonna come back and test it after lunch

--- controller/requestAction.php
+++ controller/requestAction.php
@@ -2,25 +2,29 @@
 include_once 'dbController.php';
 include_once 'requestActionController.php';
 
-// extract values from the url
-
-$sql = "SELECT * FROM inviteTable WHERE id1='$id1' AND groupid='$groupid' AND hash='$hash'";
 $conn = connectToDB();
-$result = mysqli_query($conn, $sql);
 
-$valid = false;
-// check if the request exists in the inviteTable
-if(mysqli_num_rows($result) > 0)
-{
-    $row = mysqli_fetch_assoc($result);
-    $valid = true;
+echo "hello";
+//$sql = "UPDATE groupProfile SET users=CONCAT(users, ',$id1'), size=size+1 WHERE id='';";
+//mysqli_query($conn, $sql);
 
-    // delete that row from the inviteTable
-    deleteRowInInviteTable($id1, $groupid, $hash, $conn);
 
-    // update the group row in the groupProfile table
-    updateGroupProfile($id1, $groupid, $conn);
-
-    // update the "group" field for both individuals in the students table
-    updateUserProfiles($id1, $groupid, $conn);
-}
+//$sql = "SELECT * FROM inviteTable WHERE id1='$id1' AND groupid='$groupid' AND hash='$hash'";
+//$result = mysqli_query($conn, $sql);
+//
+//$valid = false;
+//// check if the request exists in the inviteTable
+//if(mysqli_num_rows($result) > 0)
+//{
+//    $row = mysqli_fetch_assoc($result);
+//    $valid = true;
+//
+//    // delete that row from the inviteTable
+//    deleteRowInInviteTable($id1, $groupid, $hash, $conn);
+//
+//    // update the group row in the groupProfile table
+//    $result1 = updateGroupProfile($id1, $groupid, $conn);
+//
+//    // update the "group" field for both individuals in the students table
+//    updateUserProfiles($id1, $groupid, $conn);
+//}
