ee6ad4a60b4b624aefce783e880c16505189c769
p6668 <randall764992309@hotmail.com>
2017-03-03 14:31:33 -0800

    what

--- js/group-validation.js
+++ js/group-validation.js
@@ -1,5 +1,5 @@
 $().ready(function () {
-	 var minSize = <?php echo json_encode($group->getSize()); ?>;
+	//var minSize = <?php echo json_encode($group->getSize()); ?>;
     $("#editGroupForm").validate({
         // Specify validation rules
         rules: {
@@ -12,7 +12,7 @@ $().ready(function () {
             },
             size: {
                 required: true,
-                min: function() { return minSize; },
+                min: 3,
                 max: 10
             }
         },
