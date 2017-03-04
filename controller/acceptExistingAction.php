3f9237c18d5148cc2f50b63a8ace01301062c1f6
yil667 <yil667@ucsd.edu>
2017-03-04 14:14:24 -0800

    added existing action and controller, untested

---
+++ controller/acceptExistingAction.php
@@ -0,0 +1,28 @@
+<?php
+include_once 'dbController.php';
+include_once 'acceptExistingActionController.php';
+
+$conn = connectToDB();
+$MAX_GROUP_SIZE = 10;
+
+$sql = "SELECT * FROM inviteTable WHERE id1='$id1' AND id2='$id2' AND groupid='$groupid' AND hash='$hash'";
+$result = mysqli_query($conn, $sql);
+
+$valid = false;
+// check if the request exists in the inviteTable
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+
+    // delete that row from the inviteTable
+    deleteRowInInviteTable($id1, $id2, $groupid, $hash, $conn);
+
+    // update the group row in the groupProfile table
+    $result = updateGroupProfile($id2, $groupid, $MAX_GROUP_SIZE, $conn);
+
+    // if updated group successfully
+    if ($result) {
+        // update the "group" field for both individuals in the students table
+        updateUserProfiles($id2, $groupid, $conn);
+        $valid = true;
+    }
+}
---
+++ controller/acceptExistingActionController.php
@@ -0,0 +1,32 @@
+<?php
+// delete that row from the inviteTable
+function deleteRowInInviteTable($id1, $id2, $groupid, $hash, $conn)
+{
+    $sql = "DELETE FROM inviteTable where id1='$id1' AND id2='$id2' AND groupid='$groupid' AND hash='$hash'";
+    mysqli_query($conn, $sql);
+}
+
+// update the group row in the groupProfile table
+function updateGroupProfile($id2, $groupid, $MAX_GROUP_SIZE, $conn)
+{
+    // check to see if the group exists first
+    $sql = "SELECT * FROM groupProfile WHERE id='$groupid'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) == 0)
+        return false;
+
+    $row = mysqli_fetch_assoc($result);
+    if($row['size'] >= $MAX_GROUP_SIZE)
+        return false;
+
+    $sql = "UPDATE groupProfile SET users=CONCAT(users, ',$id2'), size=size+1 WHERE id='$groupid';";
+    mysqli_query($conn, $sql);
+    return true;
+}
+
+// update the "group" field for both individuals in the students table
+function updateUserProfiles($id2, $groupid, $conn)
+{
+    $sql = "UPDATE student SET groups=CONCAT(groups, ',$groupid') where id='$id2'";
+    mysqli_query($conn, $sql);
+}
