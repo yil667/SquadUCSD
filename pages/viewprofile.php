ffaea665eadc295ab5b01bef12b82638d5dda439
p6668 <randall764992309@hotmail.com>
2017-03-06 00:33:35 -0800

    test

--- pages/viewprofile.php
+++ pages/viewprofile.php
@@ -74,7 +74,28 @@ else {
 
             var displayButtons = <?php echo json_encode($displayButtons); ?>;
             var loggedIn = <?php echo json_encode(isLoggedIn()); ?>;
-      
+            if(loggedIn) {
+           
+
+                // initial check for the selection box (need to add more if
+                // check if ingroup full fullfull also clear error message
+                $('#invite-error').html("");
+                $('#invite-btn').prop('disabled', false);
+                $('#invite-btn').html("Send Invite");
+                var initialSelectBox = document.getElementById("groupid");
+                if (isInGroup[initialSelectBox.selectedIndex]) {
+                    $('#invite-btn').prop('disabled', true);
+                    $('#invite-error').html("The user is already in the group you selected!");
+                }
+                else if (mygroups[initialSelectBox.selectedIndex]["isFull"]) {
+                    $('#invite-error').html("Group is full. The group size will be increased by 1 if the user accepts.");
+                }
+                else if (mygroups[selectBox.selectedIndex]["isMax"]) {
+                    $('#invite-btn').prop('disabled', true);
+                    $('#invite-error').html("The group has reached its maximum capacity(10).");
+                }
+            }
+
 
             var groups = <?php echo json_encode($user->getGroups()); ?>;
             for (i = 0; i < groups.length; i++) {
