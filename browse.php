3bd3b57ba419a7fa248b5b206e2679443a6ce286
yil667 <yil667@ucsd.edu>
2017-03-08 16:41:22 -0800

    updated urldecode

--- browse.php
+++ browse.php
@@ -6,12 +6,12 @@ $url = json_encode($_SERVER['REQUEST_URI']);
 
 echo "!!";
 echo strrpos($url, "?class=") !== false;
-echo strrpos($url, "&type=" !== false;
+echo strrpos($url, "&type=") !== false;
 echo "!!";
 
 // redirects the url to homepage if not groupid foun
 if (strrpos($url, "?class=") !== false &&
-    strrpos($url, "&type=" !== false)
+    strrpos($url, "&type=") !== false
 ) {
 
     $_SESSION['class'] = $_GET['class'];
@@ -24,7 +24,8 @@ if (strrpos($url, "?class=") !== false &&
     // after this include, a variable named $result will be available, storing
     // an array of user objects or group objects depending on the request
     include_once "$_SERVER[DOCUMENT_ROOT]/controller/getSearchResultAction.php";
-} else // otherwise reset the array
+}
+else // otherwise reset the array
     $result = Array();
 
 echo "hi";
