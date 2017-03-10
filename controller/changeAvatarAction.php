9c2a31833140afdee1ffef00494855a0be867a78
yil667 <yil667@ucsd.edu>
2017-03-10 10:46:00 -0800

    testing mobile avatar

--- controller/changeAvatarAction.php
+++ controller/changeAvatarAction.php
@@ -11,49 +11,47 @@ $conn = connectToDB();
 $userid = getUserId();
 $target_dir = "$_SERVER[DOCUMENT_ROOT]/img/";
 
+$filename = "";
+// check if the user is using big browser
 if($_FILES["filename"]["error"] != UPLOAD_ERR_NO_FILE)
-    echo "1 is found";
-else
-    echo "1 is not found";
-
-
-if($_FILES["filename2"]["error"] != UPLOAD_ERR_NO_FILE)
-    echo "2 is found";
-else
-    echo "2 is not found";
+    $filename = $_FILES["filename"];
+// smaller browser
+else if ($_FILES["filename2"]["error"] != UPLOAD_ERR_NO_FILE)
+    $filename = $_FILES["filename2"];
+else // something went wrong
+    header("Location: http://www.squaducsd.com/editprofile.php?avatarfail");
 
 
 
+$suffix = pathinfo($filename["name"])["extension"];
+$file_name = "user_" . $userid . "." . $suffix;
+$target_str = $target_dir . $file_name;
 
-//$suffix = pathinfo($_FILES["filename"]["name"])["extension"];
-//$file_name = "user_" . $userid . "." . $suffix;
-//$target_str = $target_dir . $file_name;
-//
-//// this line checks for a valid image
-//$validImage = getimagesize($_FILES["filename"]["tmp_name"]);
-//
-//// check for the file size
-//$validSize = $_FILES["filename"]["size"] < 250000;
-//
-//if (!$validImage)
-//    header("Location: http://www.squaducsd.com/editprofile.php?invalidimage");
-//else if (!$validSize)
-//    header("Location: http://www.squaducsd.com/editprofile.php?invalidsize");
-//else {
-//    // put the file in the disk
-//    if (move_uploaded_file($_FILES["filename"]["tmp_name"], $target_str)) {
-//        // change permission
-//        chmod($target_str, 0777);
-//
-//        // update the database
-//        updateUserProfile($conn, $userid, $file_name);
-//
-//        header("Location: http://www.squaducsd.com/editprofile.php?avatarupdated");
-//
-//    }
-//    else {
-//        header("Location: http://www.squaducsd.com/editprofile.php?avatarfail");
-//    }
-//
-//
-//}
+// this line checks for a valid image
+$validImage = getimagesize($filename["tmp_name"]);
+
+// check for the file size
+$validSize = $filename["size"] < 250000;
+
+if (!$validImage)
+    header("Location: http://www.squaducsd.com/editprofile.php?invalidimage");
+else if (!$validSize)
+    header("Location: http://www.squaducsd.com/editprofile.php?invalidsize");
+else {
+    // put the file in the disk
+    if (move_uploaded_file($filename["tmp_name"], $target_str)) {
+        // change permission
+        chmod($target_str, 0777);
+
+        // update the database
+        updateUserProfile($conn, $userid, $file_name);
+
+        header("Location: http://www.squaducsd.com/editprofile.php?avatarupdated");
+
+    }
+    else {
+        header("Location: http://www.squaducsd.com/editprofile.php?avatarfail");
+    }
+
+
+}
