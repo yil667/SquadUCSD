64a2858d39e80396f67982e63b0055c90a76c5fd
p6668 <randall764992309@hotmail.com>
2017-03-09 18:23:21 -0800

    wat

--- editprofile.php
+++ editprofile.php
@@ -107,7 +107,9 @@ include_once "$_SERVER[DOCUMENT_ROOT]/controller/viewProfileAction.php";
                             <div class="col-md-9">
                                 <img src="img/default.jpg" style="width:128px;height:128px;" id="avatar">
                             </div>
-                            <div class="col-md-9">
+                        </div>
+                        <div class="form-group">
+                        	 <div class="col-md-9">
                             	<label for="choosefile" class="col-md-3 control-label"></label>
                         	    <label type="button" class="btn btn-primary" id="choose">
 								    <input type="file" id="filename" style="display:none">
