2d4a0351dbbda54e2db91f6c95a981b9d7f83b4c
yil667 <yil667@ucsd.edu>
2017-03-10 11:55:22 -0800

    edit group avatar testing

--- controller/changeAvatarController.php
+++ controller/changeAvatarController.php
@@ -9,6 +9,6 @@ function updateUserProfile($conn, $userid, $file_name)
 function updateGroupProfile($conn, $groupid, $file_name)
 {
     $avatarLink = "./img/" . $file_name;
-    $sql = "UPDATE grouProfile SET avatar='$avatarLink' WHERE id='$groupid'";
+    $sql = "UPDATE groupProfile SET avatar='$avatarLink' WHERE id='$groupid'";
     mysqli_query($conn, $sql);
 }
