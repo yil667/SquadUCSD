546bafb2fe219e2a5e1195029373e9a0dbd2c447
p6668 <randall764992309@hotmail.com>
2017-03-09 18:31:13 -0800

    fix

--- editprofile.php
+++ editprofile.php
@@ -109,8 +109,8 @@ include_once "$_SERVER[DOCUMENT_ROOT]/controller/viewProfileAction.php";
                             </div>
                         </div>
                         <div class="form-group">
-                        	 <div class="col-md-9">
-                            	<label for="choose" class="col-md-3 control-label"></label>
+                        	<label for="choose" class="col-md-3 control-label"></label>
+                        	<div class="col-md-9">
                         	    <label type="button" class="btn btn-primary" id="choose" name="choose">
 								    <input type="file" id="filename" style="display:none">
 								    Choose File
