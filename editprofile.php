74275363f628193a1433bf4f3568ace4b01e5bc4
p6668 <randall764992309@hotmail.com>
2017-03-09 17:50:54 -0800

    test

--- editprofile.php
+++ editprofile.php
@@ -106,10 +106,10 @@ include_once "$_SERVER[DOCUMENT_ROOT]/controller/viewProfileAction.php";
                             <label for="avatar" class="col-md-3 control-label">Avatar</label>
                             <div class="col-md-9">
                                 <img src="img/default.jpg" style="width:128px;height:128px;" id="avatar">
-                                <label class="custom-file-upload">
+                                <button type="button" class="btn btn-primary">
 								    <input type="file" id="filename" style="display:none">
-								    Custom Upload
-								</label>
+								    Choose File
+								</button>
                                 <button type="submit" class="btn btn-primary">Upload</button>
                             </div>
                         </div>
