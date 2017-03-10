83e4e9f6fc704c31b184ab89ebe38873e260c9f6
p6668 <randall764992309@hotmail.com>
2017-03-09 17:38:02 -0800

    avatar test

--- editprofile.php
+++ editprofile.php
@@ -100,6 +100,17 @@ include_once "$_SERVER[DOCUMENT_ROOT]/controller/viewProfileAction.php";
                     </h3>
                 </div>
                 <div class="panel-body">
+                	 <form class="form-horizontal" id="editProfileForm" role="form" method="POST"
+                          action="controller/changeAvatarAction.php">
+	                	 <div class="form-group">
+                            <label for="avatar" class="col-md-3 control-label">Avatar</label>
+                            <div class="col-md-9">
+                                <img src="img/default.jpg" style="width:128px;height:128px;" id="avatar">
+                                <input class="post" type="file" name="filename">
+                                <button type="submit" class="btn btn-primary">Upload</button>
+                            </div>
+                        </div>
+                    </form>
                     <form class="form-horizontal" id="editProfileForm" role="form" method="POST"
                           action="controller/editProfileAction.php">
                         <div class="form-group">
