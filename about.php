aae8b2fe822996c6dec4f8e852c7197634a81832
Wenyi Chen <wenyichen@outlook.com>
2017-03-12 15:02:18 -0700

    added links in about

--- about.php
+++ about.php
@@ -139,7 +139,7 @@ include_once "$_SERVER[DOCUMENT_ROOT]/controller/startUserSession.php";
                     </div>
                     <div class="media">
                         <div class="media-left">
-                            <img src="./img/default.jpg" class="media-object img-responsive img-thumbnail img-rounded"
+                            <img src="./img/default.png" class="media-object img-responsive img-thumbnail img-rounded"
                                  alt="Responsive image">
                         </div>
                         <div class="media-body">
