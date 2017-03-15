7f18d958b3bfc7e3761b92334b2e7ffa57159137
yil667 <yil667@ucsd.edu>
2017-03-15 15:29:46 -0700

    added more checks

--- controller/registerFormAction.php
+++ controller/registerFormAction.php
@@ -15,30 +15,36 @@ if(emptyNameFields($first, $last)){
     header("Location: http://www.squaducsd.com/register.php?emptynamefields");
 }
 
-$email = substr(mysqli_escape_string($conn, $_POST['email']), 0, $MAX_EMAIL_SIZE);
-if(!validEmail($email)){
-    header("Location: http://www.squaducsd.com/register.php?invalidemail");
+else
+{
+    $email = substr(mysqli_escape_string($conn, $_POST['email']), 0, $MAX_EMAIL_SIZE);
+    if(!validEmail($email)){
+        header("Location: http://www.squaducsd.com/register.php?invalidemail");
+    }
+    else
+    {
+        $password = substr(mysqli_escape_string($conn, $_POST['password']), 0, $MAX_PWD_SIZE);
+        $hash = md5(rand(0, 1000));
+        $duplicateEmail = existingEmail($email);
+
+        // see if account has already been created
+        if ($duplicateEmail) {
+            // notify the front end that
+            // the user email already exists in the database
+            // with the flag in the url
+            header("Location: http://www.squaducsd.com/register.php?existingemail");
+        } // Create the new user in the database
+
+        else {
+            // add the user into the database
+            addUser($email, $password, $first, $last, $hash);
+
+            // send the verification email
+            sendverificationEmail($first, $email, $hash);
+
+            // redirect to the homepage
+            header("Location: http://www.squaducsd.com/login.php?verify");
+        }
+    }
 }
 
-$password = substr(mysqli_escape_string($conn, $_POST['password']), 0, $MAX_PWD_SIZE);
-$hash = md5(rand(0, 1000));
-$duplicateEmail = existingEmail($email);
-
-// see if account has already been created
-if ($duplicateEmail) {
-    // notify the front end that
-    // the user email already exists in the database
-    // with the flag in the url
-    header("Location: http://www.squaducsd.com/register.php?existingemail");
-} // Create the new user in the database
-
-else {
-    // add the user into the database
-    addUser($email, $password, $first, $last, $hash);
-
-    // send the verification email
-    sendverificationEmail($first, $email, $hash);
-
-    // redirect to the homepage
-    header("Location: http://www.squaducsd.com/login.php?verify");
-}
