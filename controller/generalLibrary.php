2e92cee2c832812806d0dd5b2bfe6c37d6f40ad4
yil667 <yil667@ucsd.edu>
2017-03-15 18:28:42 -0700

    modified unicode encoding to take 4 bytes per character (e.g., emojis)

--- controller/generalLibrary.php
+++ controller/generalLibrary.php
@@ -10,23 +10,25 @@ $MAX_PWD_SIZE = 12;
 
 $MAX_EMAIL_SIZE = 60;
 
-// front end checks for 500 characters max, but some unicode takes 3 bytes per character
+// front end checks for 500 characters max, but some unicode takes 4 bytes per character
 // e.g., korean characters
-$MAX_MESSAGE_SIZE = 505 * 3;
+$UNICODE_BYTES = 4;
 
-$MAX_NAME_SIZE = 45 * 3;
+$MAX_MESSAGE_SIZE = 505 * $UNICODE_BYTES;
 
-$MAX_CLASS_NAME = 45 * 3;
+$MAX_NAME_SIZE = 45 * $UNICODE_BYTES;
 
-$MAX_GROUP_NAME = 45 * 3;
+$MAX_CLASS_NAME = 45 * $UNICODE_BYTES;
 
-$MAX_MAJOR_NAME = 45 * 3;
+$MAX_GROUP_NAME = 45 * $UNICODE_BYTES;
+
+$MAX_MAJOR_NAME = 45 * $UNICODE_BYTES;
 
 $MAX_PHONE_SIZE = 15;
 
-$MAX_ABOUT_SIZE = 1005 * 3;
+$MAX_ABOUT_SIZE = 1005 * $UNICODE_BYTES;
 
-$MAX_QUERY_SIZE = 100 * 3;
+$MAX_QUERY_SIZE = 100 * $UNICODE_BYTES;
 
 // this function clears all the flags in an url, after and including &
 function clearFlags($url)
